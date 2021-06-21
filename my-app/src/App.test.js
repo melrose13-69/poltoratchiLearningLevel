ee73268757b7f5eda3c9fdf1e3931c8bc26ced0e
Victor Poltoratchi <59205832+melrose13-69@users.noreply.github.com>
2021-06-21 12:06:11 +0300

    Delete my-app directory
    delete dev folder

--- my-app/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import MainApp from './App';
-import * as ReactDOM from 'react-dom';
-
-test('renders learn react link', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MainApp/>, div);
-  ReactDOM.unmountComponentAtNode(div)
-});
--- my-app/src/components/common/preloader/Preloader.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import preloader from '../../../assets/images/loader.svg';
-
-
-const Preloader = props => {
-    return <div><img src={preloader} alt='preloader'/></div>
-};
-
-export default Preloader;
